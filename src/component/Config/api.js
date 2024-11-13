77f4c532678efed026e910bf99512bff0da6b9a2
ThomasNguyen <tienanhnguyen999@gmail.com>
2024-11-13 21:30:38 +0700

    Your commit message

--- src/component/Config/api.js
+++ src/component/Config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const API_URL = "http://localhost:8080";
-export const API_URL = "foodonlinebackend-production.up.railway.app";
+export const API_URL = "http://localhost:8080";
+// export const API_URL = "foodonlinebackend-production.up.railway.app";
 
 export const api = axios.create({
     baseURL: API_URL,
